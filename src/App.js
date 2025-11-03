ff6714743b052cf11c1cd664aba3e970f5faff6d
Nemanja Stankovic <nemanja.stankovic@shindiristudio.com>
2025-11-03 09:33:40 +0100

    fixed: focus not showing up after separating homePage from App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import { Lightning, Utils } from "@lightningjs/sdk";
-import ContentSection from "./components/ContentSection";
-import TopChannels from "./components/TopChannels";
-import LivePlayerButton from "./components/LivePlayerButton";
+import HomePage from "./pages/HomePage";
 
-const topChannelsData = [
-  { name: "CBS", image: "CBS.png" },
-  { name: "NBC", image: "NBC.png" },
-  { name: "ABC", image: "ABC.png" },
-  { name: "Fox", image: "FOX.png" },
-  { name: "Fox News Channel", image: "FNC.png" },
-];
 export default class App extends Lightning.Component {
   static getFonts() {
     return [
@@ -26,98 +17,10 @@ export default class App extends Lightning.Component {
   }
 
   static _template() {
-    return {
-      Background: {
-        w: 776,
-        h: 1080,
-        color: 0xff151515,
-        rect: true,
-      },
-      GradientTexture: {
-        x: 776,
-        y: 0,
-        w: 1920,
-        h: 1080,
-        src: Utils.asset("images/background.jpg"),
-        clippg: true,
-      },
-      RectangleWithGradientDiagonal: {
-        x: 776,
-        y: 0,
-        w: 1144,
-        h: 1080,
-        rect: true,
-        colorRight: 0x00000000,
-        colorLeft: 0xff151515,
-      },
-      Content: {
-        x: 64,
-        y: 125,
-        w: 1241,
-        h: 1080,
-        type: ContentSection,
-      },
-      TopChannels: {
-        x: 1415,
-        y: 122,
-        rect: true,
-        type: TopChannels,
-        props: topChannelsData,
-      },
-      LivePlayerButton: {
-        type: LivePlayerButton,
-      },
-    };
+    return { HomePage: { type: HomePage } };
   }
 
-  _init() {
-    this._setState("Content");
-  }
-
-  get _Content() {
-    return this.tag("Content");
-  }
-  get _TopChannels() {
-    return this.tag("TopChannels");
-  }
-
-  get _LivePlayerButton() {
-    return this.tag("LivePlayerButton");
-  }
-
-  static _states() {
-    return [
-      class Content extends this {
-        _getFocused() {
-          return this._Content;
-        }
-        _handleRight() {
-          this._setState("TopChannels");
-          return true;
-        }
-        _handleDown() {
-          this._setState("LivePlayerButton");
-          return true;
-        }
-      },
-      class TopChannels extends this {
-        _getFocused() {
-          return this._TopChannels;
-        }
-        _handleLeft() {
-          this._setState("Content");
-          return true;
-        }
-      },
-      class LivePlayerButton extends this {
-        _getFocused() {
-          return this._LivePlayerButton;
-        }
-        _handleUp() {
-          this._setState("Content");
-          return true;
-        }
-      },
-    ];
+  _getFocused() {
+    return this.tag("HomePage");
   }
 }
